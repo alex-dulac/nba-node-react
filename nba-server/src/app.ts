7acb8a9a89b74f13c3950373b9f7361b1478013b
Alex Dulac <alex-dulac@idexx.com>
2022-08-14 10:15:59 -0400

    bootstrapped node.js with nba api

--- nba-server/src/app.ts
+++ nba-server/src/app.ts
@@ -1,1 +1,26 @@
-console.log('Test');
+import express, {json, Request, Response} from 'express';
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api-nba-v1.p.rapidapi.com/seasons',
+        headers: {
+            'X-RapidAPI-Key': 'KEY',
+            'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+        }
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+});
+
+app.listen(4000, () => {
+    console.log("Backend application listening at port 4000...")
+});
